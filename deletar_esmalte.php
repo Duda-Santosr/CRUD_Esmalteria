3c5a194d7e6dbb74063eb75221cc26ebc81aafe4
Duda-Santosr <maria.e.santosr13@gmail.com>
2025-10-06 11:09:42 -0300

    commit

--- deletar_esmalte.php
+++ deletar_esmalte.php
@@ -15,10 +15,15 @@ include 'config.php';
 $id = $_GET['id'] ?? 0;
 
 if ($id > 0) {
-    // Prepara uma consulta segura para deletar a pizza
+    $stmt = $conn->prepare("DELETE FROM movimentacoes WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $stmt->close();
+    // Prepara uma consulta segura para deletar o esmalte
     $stmt = $conn->prepare("DELETE FROM esmaltes WHERE id = ?");
     $stmt->bind_param("i", $id);
     $stmt->execute();
+    $stmt->close();
 }
 
 // Redireciona para a lista de esmaltes
